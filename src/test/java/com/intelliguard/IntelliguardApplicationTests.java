c8a004917021b77ca00f1f07a20f133c2b0e756c
Shoheb <shohebtalha@gmail.com>
2026-06-15 13:55:23 +0530

    Init springboot project with maven and core dependencies

---
+++ src/test/java/com/intelliguard/IntelliguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intelliguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelliguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
